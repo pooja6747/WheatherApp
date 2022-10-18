de60010da8df7450fb93388a441f7e2827fb5170
pooja <pooja@>
2022-10-18 09:58:13 +0530

    error boundries

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Whether from './Component/Whether';
+import ErrorBoundries from './ErrorBoundries';
 
 function App() {
   return (
     <>
+    <ErrorBoundries>
      <Whether/>
+     </ErrorBoundries>
    </>
   );
 }
---
+++ src/ErrorBoundries.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ErrorBoundries = () => {
+
+  return (
+    <>
+        <h1>Error boundary</h1>
+    </>
+  )
+}
+
+export default ErrorBoundries
